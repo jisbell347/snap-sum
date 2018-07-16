8a35248dd5218023fbb36c9cc5901109fdeef7ee
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-16 08:56:04 -0600

    Changed function and added old way of doing it

--- snap-sum.js
+++ snap-sum.js
@@ -1,4 +1,27 @@
-let arr = [5, 6, 7, 8];
-let sum = (accumulator, currentValue) => accumulator + currentValue;
+// let arr = [5, 6, 7, 8];
+// let sum = (accumulator, currentValue) => accumulator + currentValue;
+//
+// console.log(arr.reduce(sum)); //Expected output: 26
+/**
+* @param {array} arrayToSum array of numrics to sum
+* @returns {number} value of sum
+ **/
 
-console.log(arr.reduce(sum)); //Expected output: 26
+function sumArrayOldSchool(arrayToSum) {
+	let sum = 0;
+	for(let i = 0; i, arrayToSum.length; i++) {
+		sum = sum + arrayToSum[i];
+	}
+
+	return sum;
+}
+
+/**
+ *
+ * @param {array} arrayToSum array of numerics to sum
+ * @returns {number} value of sum
+ */
+
+function sumArrayFunctional(arrayToSum) {
+	return arrayToSum.reduce((sum, current) => sum + current);
+}
