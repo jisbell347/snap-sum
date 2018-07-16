3a9eee7a15d51537383dc7f9a2bb53abba1c6601
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-16 08:56:12 -0600

    Changed function and added old way of doing it

--- snap-sum.php
+++ snap-sum.php
@@ -1,4 +1,24 @@
 <?php
-$array = array(10, 20, 30);
-echo array_sum($array); //Expected Output: 60;
+/**
+ * sums an array old school
+ *
+ * @param array $arrayToSum array of numerics to sum
+ * @return int|float value of sum
+ **/
+function sumArrayOldSchool(array $arrayToSum) {
+	$sum = 0;
+	forEach($arrayToSum as $item) {
+		$sum = $sum + $item;
+	}
+	return $sum;
+}
+/**
+ * sums an array the easy way
+ *
+ * @param array $arrayToSum array of numerics to sum
+ * @return float|int of sum
+ **/
+function sumArrayEasyWay(array $arrayToSum) {
+	return array_sum($arrayToSum);
+}
 ?>
